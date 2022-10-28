b7b236364eb981f0364312e6cb20c9bfba6d2c5f
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-10-28 17:54:17 -0400

    Extend index.js to non-root pages

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -5,7 +5,7 @@ window.addEventListener('resize', function () {
 
 // Tilt effect
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
-if (!prefersReducedMotion || !prefersReducedMotion.matches) {
+if ((!prefersReducedMotion || !prefersReducedMotion.matches) && typeof VanillaTilt !== 'undefined') {
     VanillaTilt.init(document.querySelectorAll('.project-card'), {
         glare: true,
         'max-glare': .5,
@@ -16,6 +16,11 @@ if (!prefersReducedMotion || !prefersReducedMotion.matches) {
 // Language selector
 const languageSelect = document.querySelector('.language-select');
 languageSelect.addEventListener('change', () => {
-    const folder = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
-    window.location = `${folder}/${languageSelect.value}`;
+    const currLocation = window.location.pathname.substring(1, window.location.pathname.length);
+    if (currLocation.includes('/')) {
+        const path = currLocation.substring(currLocation.indexOf('/'), currLocation.length);
+        window.location = `/${languageSelect.value}${path}`;
+    } else {
+        window.location = `/${languageSelect.value}`;
+    }
 });
